1322c68897acd52151f984c7471dd4a8596a6c83
kazu0112 <kazuma0112@icloud.com>
2026-06-26 17:45:05 +0900

    ecsite.sql修正

--- sql/ecsite.sql
+++ sql/ecsite.sql
@@ -31,8 +31,8 @@ create table user_buy_item_transaction(
 	item_transaction_id int,
 	total_price int,
 	total_count int,
-	user_master_id varchar(16);
-	pay varchar(30);
+	user_master_id varchar(16),
+	pay varchar(30),
 	insert_date datetime,
 	delete_date datetime
 );
